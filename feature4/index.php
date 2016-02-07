bb93b4cfb71ffb44fdaf051af2432b805e6eb77e
tomasz kowalczyk <tomaszkowalczyk@eorigen.com>
2016-02-07 14:20:55 +0000

    feat4: create skeleton

---
+++ feature4/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Feature 4</h1>
+	<h2>ver 0.1</h2>
+	<h3>author: tomasz kowalczyk</h3>
+	<h4>date: 07/02/2016</h4>
+</body>
+</html>
