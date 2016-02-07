65ba29dd003c3a0ddd4921c66ace99f7e325071e
tomasz kowalczyk <tomaszkowalczyk@eorigen.com>
2016-02-07 13:20:07 +0000

    f2: working on skeleton

--- feature2/index.php
+++ feature2/index.php
@@ -1,6 +1,13 @@
-<?php  
-
-echo "<p>hello!</p>";
-echo "<p>This is my first feature</p>";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Feature 2</h1>
+	<h2>ver 0.2</h2>
+	<h3>author: tomasz kowalczyk</h3>
+	<h4>date: 07/02/2016</h4>
+</body>
+</html>
